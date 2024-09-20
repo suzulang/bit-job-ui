4d89f618b82c229ada2b6eb399add2f8b9d1c0dc
suzulang <suzulang.jiang@gmail.com>
2024-09-20 22:30:55 +0800

    Delete unused imported code

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
